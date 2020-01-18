fda953d0c748203463b474ee8602fa4ac5dc3ad5
unknown <@WS-0099.mgtniip.ru>
2020-01-19 00:57:15 +0300

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import CountTo from './vue-countTo.vue';
 export default CountTo;
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('count-to', CountTo);
+
+if ( typeof window !== 'undefined' && window.Vue ) {
+
+    window.Vue.component( 'count-to', CountTo );
+
 }
--- src/requestAnimationFrame.js
+++ src/requestAnimationFrame.js
@@ -1,46 +1,80 @@
-let lastTime = 0
-const prefixes = 'webkit moz ms o'.split(' ') // 各浏览器前缀
+let lastTime = 0;
+const prefixes = 'webkit moz ms o'.split(' '); // Browser prefixes.
 
-let requestAnimationFrame
-let cancelAnimationFrame
+let requestAnimationFrame;
+let cancelAnimationFrame;
 
-const isServer = typeof window === 'undefined'
-if (isServer) {
-  requestAnimationFrame = function() {
-    return
-  }
-  cancelAnimationFrame = function() {
-    return
-  }
+const isServer = typeof window === 'undefined';
+
+if ( isServer ) {
+
+    requestAnimationFrame = function() {
+
+        return;
+
+    }
+
+    cancelAnimationFrame = function() {
+
+        return;
+
+    }
+
 } else {
-  requestAnimationFrame = window.requestAnimationFrame
-  cancelAnimationFrame = window.cancelAnimationFrame
-  let prefix
-    // 通过遍历各浏览器前缀，来得到requestAnimationFrame和cancelAnimationFrame在当前浏览器的实现形式
-  for (let i = 0; i < prefixes.length; i++) {
-    if (requestAnimationFrame && cancelAnimationFrame) { break }
-    prefix = prefixes[i]
-    requestAnimationFrame = requestAnimationFrame || window[prefix + 'RequestAnimationFrame']
-    cancelAnimationFrame = cancelAnimationFrame || window[prefix + 'CancelAnimationFrame'] || window[prefix + 'CancelRequestAnimationFrame']
-  }
 
-  // 如果当前浏览器不支持requestAnimationFrame和cancelAnimationFrame，则会退到setTimeout
-  if (!requestAnimationFrame || !cancelAnimationFrame) {
-    requestAnimationFrame = function(callback) {
-      const currTime = new Date().getTime()
-      // 为了使setTimteout的尽可能的接近每秒60帧的效果
-      const timeToCall = Math.max(0, 16 - (currTime - lastTime))
-      const id = window.setTimeout(() => {
-        callback(currTime + timeToCall)
-      }, timeToCall)
-      lastTime = currTime + timeToCall
-      return id
+    requestAnimationFrame = window.requestAnimationFrame;
+    cancelAnimationFrame = window.cancelAnimationFrame;
+
+    let prefix;
+
+    // By traversing the browser prefixes, get the implementation
+    // form of requestAnimationFrame and cancelAnimationFrame
+    // in the current browser.
+    for ( let i = 0; i < prefixes.length; i++ ) {
+
+        if ( requestAnimationFrame && cancelAnimationFrame ) {
+
+            break;
+
+        }
+
+        prefix = prefixes[ i ];
+        requestAnimationFrame = requestAnimationFrame || window[ prefix + 'RequestAnimationFrame' ];
+        cancelAnimationFrame = cancelAnimationFrame || window[ prefix + 'CancelAnimationFrame' ] || window[ prefix + 'CancelRequestAnimationFrame' ];
+
     }
 
-    cancelAnimationFrame = function(id) {
-      window.clearTimeout(id)
+    // If the current browser does not support requestAnimationFrame
+    // and cancelAnimationFrame, it will fall back to setTimeout.
+    if ( !requestAnimationFrame || !cancelAnimationFrame ) {
+
+        requestAnimationFrame = function( callback ) {
+
+            const currTime = new Date().getTime();
+
+            // In order to make setTimeout as close as possible
+            // to 60 frames per second.
+            const timeToCall = Math.max( 0, 16 - ( currTime - lastTime ) );
+            const id = window.setTimeout( () => {
+
+                callback( currTime + timeToCall )
+
+            }, timeToCall );
+
+            lastTime = currTime + timeToCall;
+
+            return id;
+
+        }
+
+        cancelAnimationFrame = function( id ) {
+
+            window.clearTimeout( id )
+
+        }
+
     }
-  }
+
 }
 
-export { requestAnimationFrame, cancelAnimationFrame }
+export { requestAnimationFrame, cancelAnimationFrame };
